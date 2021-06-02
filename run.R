cd5e2ced4cd7b5d93b98328b88d344e8bd408183
vbinimelis <79914232+vbinimelis@users.noreply.github.com>
2021-06-02 17:35:52 +0200

    Update run.R

--- run.R
+++ run.R
@@ -1,10 +1,11 @@
-# run.R
-library(shiny)
-
 port <- Sys.getenv('PORT')
 
-shiny::runApp(
-  appDir = getwd(),
-  host = '0.0.0.0',
-  port = as.numeric(port)
+setwd('/app')
+
+rmarkdown::run(
+  'LAG_RUG_DASH_v0.2.Rmd'
+  , shiny_args = list(
+    host = '0.0.0.0',
+    port = as.numeric(port)
+  )
 )
